6fbac95f10976a42b9685620f19c0b000fc7d452
MrBoboGet <emanuelberggren01@gmail.com>
2022-06-15 18:10:26 +0200

    Initial commit

---
+++ Assets/Code/RuleManager/RuleManager.cs.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace RuleManager
+{
+    enum TargetType
+    {
+        Player,
+        Unit,
+        Tile,
+    }
+    class TargetInfo
+    {
+          
+    }
+    enum ActionType
+    {
+        Move,
+        UnitEffect,
+        Stratagem
+    }
+    class Action
+    {
+        public ActionType Type;
+    }
+
+    class UnitID
+    {
+
+    }
+    enum EffectType
+    {
+        Continous,
+        Triggered,
+        Actived
+    }
+    class UnitInfo
+    {
+        public EffectType Temp = EffectType.Actived;
+    }
+
+    class RuleManager
+    {
+
+
+        public UnitInfo GetUnitInfo(UnitID ID)
+        {
+            return (new UnitInfo());
+        }
+    }
+}
---
+++ Assets/TempTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RuleManager;
+
+public class TempTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        RuleManager.RuleManager Ruler = new RuleManager.RuleManager();
+        print(Ruler.GetUnitInfo(new UnitID()).Temp.ToString());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
